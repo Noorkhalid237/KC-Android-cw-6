a3a91ef4df7eecd488b1834678883d1b63a781e4
Noorkhalid237 <109866546+Noorkhalid237@users.noreply.github.com>
2022-10-26 20:17:00 +0300

    Created Android school

--- AndroidSchool/app/src/main/java/com/mohammedev/androidschool/MainActivity.java
+++ AndroidSchool/app/src/main/java/com/mohammedev/androidschool/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
      *  عرفوا مصفوفة بإسم students
      *  define an arraylist named students
      */
+    ArrayList<Student> students = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
          *  ضيفوا عناصر من الكلاس
          *  add objects from class Student
          */
+
+        Student s1 = new Student("Noor",14,12,R.drawable.girl);
+        Student s2 = new Student("Maryam",13,10,R.drawable.girl1);
+        Student s3 = new Student("Sara",15,11,R.drawable.girl2);
+
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
 
 
         StudentAdapter studentAdapter = new StudentAdapter(this,0,students);
